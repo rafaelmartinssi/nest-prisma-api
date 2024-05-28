0ce3c111cdbbd291642e79d55f92da6f0e013e19
Rafael Martins Galdino <rafael_martins_182@hotmail.com>
2024-05-27 23:28:14 -0300

    ClassValidatorFields  unit tests

---
+++ src/users/domain/validators/__tests__/unit/class-validator-fields.spec.ts
@@ -0,0 +1,27 @@
+import { ClassValidatorFields } from '../../class-validator-fields'
+import * as libClassValidator from 'class-validator'
+
+class StubClassValidatorFields extends ClassValidatorFields<{
+  field: string
+}> {}
+
+describe('ClassValidatorFields unit tests', () => {
+  it('Should initialize errors and validatedData variables with null', () => {
+    const sut = new StubClassValidatorFields()
+    expect(sut.errors).toBe(null)
+    expect(sut.validatedData).toBe(null)
+  })
+
+  it('Should validated with errors', () => {
+    const spyValidateSync = jest.spyOn(libClassValidator, 'validateSync')
+    spyValidateSync.mockReturnValue([
+      { property: 'field', constraints: { isRequired: 'test error' } },
+    ])
+    const sut = new StubClassValidatorFields()
+
+    expect(sut.validate(null)).toBeFalsy()
+    expect(spyValidateSync).toHaveBeenCalled()
+    expect(sut.validatedData).toBe(null)
+    expect(sut.errors).toStrictEqual({ field: ['test error'] })
+  })
+})
