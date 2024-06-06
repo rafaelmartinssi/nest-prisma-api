d3bded70a42681b6299d9868e8e742af612f4b40
Rafael Martins Galdino <rafael_martins_182@hotmail.com>
2024-06-05 22:23:44 -0300

    Add SearchParams class getters and setters

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -29,23 +29,58 @@ export class SearchParams {
   get page() {
     return this._page
   }
-  private set page(page: number) {}
+  private set page(page: number) {
+    let _page = +page
+    if (Number.isNaN(_page) || _page <= 0 || parseInt(_page as any) !== _page) {
+      _page = 1
+    }
+    this._page = _page
+  }
+
   get perPage() {
     return this._perPage
   }
-  private set perPage(perPage: number) {}
+  private set perPage(perPage: number) {
+    let _perPage = +perPage
+    if (
+      Number.isNaN(_perPage) ||
+      _perPage <= 0 ||
+      parseInt(_perPage as any) !== _perPage
+    ) {
+      _perPage = this._perPage
+    }
+    this._perPage = _perPage
+  }
+
   get sort() {
     return this._sort
   }
-  private set sort(sort: string | null) {}
+  private set sort(sort: string | null) {
+    this.sort =
+      sort === null || sort === undefined || sort === '' ? null : String(sort)
+  }
+
   get sortDir() {
     return this._sortDir
   }
-  private set sortDir(sortDir: string | null) {}
+  private set sortDir(sortDir: string | null) {
+    if (!this.sort) {
+      this.sortDir = null
+      return
+    }
+    const dir = String(sortDir).toLocaleLowerCase()
+    this._sortDir = dir !== 'asc' && dir !== 'desc' ? 'desc' : dir
+  }
+
   get filter() {
     return this._filter
   }
-  private set filter(filter: string | null) {}
+  private set filter(filter: string | null) {
+    this.sort =
+      filter === null || filter === undefined || filter === ''
+        ? null
+        : String(filter)
+  }
 }
 
 export interface SearchableRepositoryInterface<
