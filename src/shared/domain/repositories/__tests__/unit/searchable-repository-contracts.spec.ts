a44021ed48baed371a1594f69193ce66e88e1c25
Rafael Martins Galdino <rafael_martins_182@hotmail.com>
2024-06-08 19:28:18 -0300

    SearchParams unit tests props sort, sortDir and filter

--- src/shared/domain/repositories/__tests__/unit/searchable-repository-contracts.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/searchable-repository-contracts.spec.ts
@@ -31,5 +31,69 @@ describe('Searchable Repository unit tests', () => {
 
       expect(new SearchParams({ perPage: 25 }).perPage).toBe(25)
     })
+
+    it('sort prop', () => {
+      expect(new SearchParams({ sort: null }).sort).toBeNull()
+      expect(new SearchParams({ sort: undefined }).sort).toBeNull()
+      expect(new SearchParams({ sort: '' }).sort).toBeNull()
+
+      expect(new SearchParams({ sort: 'field' }).sort).toBe('field')
+    })
+
+    it('sortDir prop', () => {
+      expect(new SearchParams({ sort: null }).sortDir).toBeNull()
+      expect(new SearchParams({ sort: undefined }).sortDir).toBeNull()
+      expect(new SearchParams({ sort: '' as any }).sortDir).toBeNull()
+      expect(
+        new SearchParams({
+          sort: 'field',
+          sortDir: null,
+        }).sortDir,
+      ).toBe('desc')
+      expect(
+        new SearchParams({
+          sort: 'field',
+          sortDir: undefined,
+        }).sortDir,
+      ).toBe('desc')
+      expect(
+        new SearchParams({
+          sort: 'field',
+          sortDir: '' as any,
+        }).sortDir,
+      ).toBe('desc')
+      expect(
+        new SearchParams({
+          sort: 'field',
+          sortDir: 'desc',
+        }).sortDir,
+      ).toBe('desc')
+      expect(
+        new SearchParams({
+          sort: 'field',
+          sortDir: 'DESC' as any,
+        }).sortDir,
+      ).toBe('desc')
+      expect(
+        new SearchParams({
+          sort: 'field',
+          sortDir: 'asc',
+        }).sortDir,
+      ).toBe('asc')
+      expect(
+        new SearchParams({
+          sort: 'field',
+          sortDir: 'ASC' as any,
+        }).sortDir,
+      ).toBe('asc')
+    })
+
+    it('filter prop', () => {
+      expect(new SearchParams({ filter: null }).filter).toBeNull()
+      expect(new SearchParams({ filter: undefined }).filter).toBeNull()
+      expect(new SearchParams({ filter: '' }).filter).toBeNull()
+
+      expect(new SearchParams({ filter: 'field' }).filter).toBe('field')
+    })
   })
 })
