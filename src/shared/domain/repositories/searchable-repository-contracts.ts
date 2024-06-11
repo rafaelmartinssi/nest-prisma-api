9a2f26ca4192c8541bb615a22c5072c76281d813
Rafael Martins Galdino <rafael_martins_182@hotmail.com>
2024-06-10 22:51:58 -0300

    Refactor user repository

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -21,14 +21,14 @@ export type SearchResultProps<E extends Entity, Filter> = {
   filter: Filter | null
 }
 
-export class SearchParams {
+export class SearchParams<Filter = string> {
   protected _page: number
   protected _perPage = 15
   protected _sort: string | null
   protected _sortDir: SortDirection | null
-  protected _filter: string | null
+  protected _filter: Filter | null
 
-  constructor(props: SearchProps = {}) {
+  constructor(props: SearchProps<Filter> = {}) {
     this.page = props.page
     this.perPage = props.perPage
     this.sort = props.sort
@@ -82,14 +82,14 @@ export class SearchParams {
     this._sortDir = dir !== 'asc' && dir !== 'desc' ? 'desc' : dir
   }
 
-  get filter() {
+  get filter(): Filter | null {
     return this._filter
   }
-  private set filter(filter: string | null) {
+  private set filter(filter: Filter | null) {
     this._filter =
       filter === null || filter === undefined || filter === ''
         ? null
-        : String(filter)
+        : (`${filter}` as any)
   }
 }
 
@@ -131,7 +131,7 @@ export class SearchResult<E extends Entity, Filter = string> {
 export interface SearchableRepositoryInterface<
   E extends Entity,
   Filter = string,
-  SearchInput = SearchParams,
+  SearchInput = SearchParams<Filter>,
   SearchOutput = SearchResult<E, Filter>,
 > extends RepositoryInterface<E> {
   sortableFields: string[]
--- src/users/domain/repositories/user.repository.ts
+++ src/users/domain/repositories/user.repository.ts
@@ -1,8 +1,22 @@
 import { UserEntity } from '../entities/user.entity'
-import { SearchableRepositoryInterface } from '@/shared/domain/repositories/searchable-repository-contracts'
+import {
+  SearchParams,
+  SearchResult,
+  SearchableRepositoryInterface,
+} from '@/shared/domain/repositories/searchable-repository-contracts'
 
+export type Filter = string
+
+export class UserSearchParams extends SearchParams<Filter> {}
+
+export class UserSearchResult extends SearchResult<UserEntity, Filter> {}
 export interface UserRepository
-  extends SearchableRepositoryInterface<UserEntity, any, any> {
+  extends SearchableRepositoryInterface<
+    UserEntity,
+    Filter,
+    UserSearchParams,
+    UserSearchResult
+  > {
   findByEmail(email: string): Promise<UserEntity>
   emailExists(email: string): Promise<void>
 }
